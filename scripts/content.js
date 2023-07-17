a67a9e68c8c3e700cce99933f5d0b9dda6c33f39
Indred <ahmed.idrees3110@gmail.com>
2023-07-17 01:04:28 -0500

    Add popup feature when hovering on movie

--- scripts/content.js
+++ scripts/content.js
@@ -1,25 +1,63 @@
 
 console.log("CONTENT SCRIPT LOADED");
 
-let movieName;
+const popup = document.createElement("div");
+const header = document.createElement("h1");
 
-function getMovieName(movieIMG) {
-    console.log("hovered");
-    console.log(movieIMG.nextElementSibling.textContent);
+function showPopup(movieIMG, movieName) {
+
+    popup.className = "info-popup";
+
+    popup.style.position = 'fixed';
+    popup.style.display = "block";
+    popup.style.border = "1px solid #ccc";
+    popup.style.padding = "10px";
+    popup.style.color = 'black';
+    popup.style.background = 'rgba(255, 255, 255, 0.8)';
+    popup.style.zIndex = "999";
+    popup.style.borderRadius = '6 px';
+
+    const imageRect = movieIMG.getBoundingClientRect();
+
+    const infoTop = imageRect.top;
+    const infoLeft = imageRect.left;
+
+    popup.style.top = infoTop/2.5 + 'px'; // change as needed later
+    popup.style.left = infoLeft - (imageRect.width/1.5)/2 + 'px';
+    popup.style.width = imageRect.width*1.5 + 'px';
+
+
+
+    header.textContent = movieName;
+    header.style.textAlign = "center";
+    popup.appendChild(header);
+
+    
+    document.body.appendChild(popup);
+
+
 }
 
 function detectHover() {
     let movieIMGs = document.querySelectorAll(".boxart-image");
     try {
         movieIMGs.forEach((movieIMG) => {
-            movieIMG.onmouseover = () => {
-                setTimeout(() => {
-                console.log("hovered");
-                console.log(movieIMG.nextElementSibling.textContent);
-                movieName = movieIMG.nextElementSibling.textContent;
-                const response = chrome.runtime.sendMessage({movieName: movieName});
-                console.log(response);
-                }, 1000);
+            movieIMG.onmouseenter = () => {
+                const hoverTimer = setTimeout(() => {
+                    console.log("hovered");
+                    
+                    console.log(movieIMG.nextElementSibling.textContent);
+
+                    let movieName = movieIMG.nextElementSibling.textContent;
+                    const response = (async () => await chrome.runtime.sendMessage({movieName: movieName}));
+
+                    showPopup(movieIMG, movieName);
+                }, 1500);
+                movieIMG.onmouseleave = () => {
+                    //clearTimeout(hoverTimer);
+                    popup.style.display = 'none';
+                    console.log("off");
+                }
             }
         });
     } catch (error) {
@@ -36,4 +74,8 @@ const observer = new MutationObserver((mutations) => {
     }
 });
 
+
 observer.observe(document.body, { childList: true, subtree: true });
+
+
+
